f80ea22a653c9dd9a2d3de97f8f21be15792c7bf
tekthiyagu <tekthiyagu@gmail.com>
2023-09-23 20:07:15 +0530

    prototypechain-1

---
+++ js-prototypechain.js
@@ -0,0 +1,11 @@
+// prototype chain:
+
+// Prototype chaining is used to build new types of objects based on existing ones. It is similar to inheritance in a class based language.
+
+// The prototype on object instance 
+
+object.getPrototypeOf(object)
+
+// constructor function:
+
+Object.prototype();
